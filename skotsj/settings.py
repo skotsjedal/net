f33f39504568a2a32b6d4b203b450e3a1c2c08a7
Skotsjedal <andreas.skomedal@gmail.com>
2013-10-11 20:08:00 +0200

    gitignore, duh

--- skotsj/settings.py
+++ skotsj/settings.py
@@ -1,5 +1,6 @@
 # Django settings for skotsj project.
-
+from privatesettings import *
+ 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -9,17 +10,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -81,9 +71,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'ctcuhgg8boxf_yw^t27g*y@@_-wwu8g&ic%x*#!hp0&8jy00yr'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
