7449ab778bd49e99eaca5a90dab9a7e82cf22d51
andreas <aokaado@gmail.com>
2013-10-17 12:53:40 +0200

    Markdown code highlighting

--- blog/templatetags/markdown.py
+++ blog/templatetags/markdown.py
@@ -6,9 +6,11 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.filter(is_safe=True)
 @stringfilter
 def convert_text(value):
     return mark_safe(markdown2.markdown(force_unicode(value), extras=["fenced-code-blocks"]))
+
 
 register.filter('markdown', convert_text)
